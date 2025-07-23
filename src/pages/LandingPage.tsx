d758bc3436c831ca93c082c2af9ab292c94aa1bd
julianmoder <julianmo@live.de>
2025-07-23 02:21:30 +0200

    style(interim): set up interim design

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -12,27 +12,31 @@ export default function LandingPage({ onEnterTavern }: { onEnterTavern: (hero: s
   const isHeroValid = hero.trim().length > 0;
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-6 text-4xl text-white mx-auto">
-      <p className="mb-3 font-bold">What is your name, weary traveler?</p>
+    <>
+
+      <p className="mb-3 text-center font-bold">What is your name, weary traveler?</p>
 
       <div className="flex space-x-2 mt-6">
-        <input className="p-2 rounded-x1 text-center text-white font-normal border-solid border-2 leading-1 rounded-xl"
+
+        <input className="p-2 rounded-x1 text-center text-amber-400 focus:text-amber-400 focus:outline-amber-400 focus:outline-2 font-semibold border-solid border-3 leading-1 rounded-xl"
           type="text"
           value={hero}
           onChange={(e) => setHero(e.target.value)}
           placeholder="Enter your hero's name"
         />
+
         <button
           className="hover:bg-stone-600 text-white px-3 rounded-full font-normal text-4xl"
           onClick={genHero}
         >
           🎲
         </button>
+
       </div>
 
-      <button className={`mt-18 px-9 py-3 font-normal rounded-full 
+      <button className={`mt-18 px-9 py-3 font-normal text-amber-900 rounded-full 
         ${ isHeroValid
-            ? 'bg-green-500 hover:bg-green-600'
+            ? 'bg-amber-400 hover:bg-amber-500'
             : 'bg-gray-500 cursor-not-allowed'
         }`}
         onClick={() => isHeroValid && onEnterTavern(hero.trim())}
@@ -40,6 +44,7 @@ export default function LandingPage({ onEnterTavern }: { onEnterTavern: (hero: s
       >
         Enter the Tavern
       </button>
-    </div>
+
+    </>
   );
 }
